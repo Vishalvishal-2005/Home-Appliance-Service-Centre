b6a5063064da6337f103985c1c899a5b60e35bec
unknown <727822tuad061@skct.edu.in>
2024-11-28 18:57:59 +0530

    Success

--- novend.js
+++ novend.js
@@ -1,11 +1,10 @@
 import React from 'react';
 const Login =() => {
-
   return (
     <div>
-      <h1>Hello</h1>
+        <h1>Hello</h1>
+        <h1>Hello</h1>
     </div>
   );
 };
-
 export default Login;
